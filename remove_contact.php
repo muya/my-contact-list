c0b235a2fea7aebfea10c1323c61e780f4e1515a
muya <kingkonig@gmail.com>
2013-11-02 11:18:45 +0300

    added remove & update functionality

---
+++ remove_contact.php
@@ -0,0 +1,107 @@
+<?php
+require_once (dirname(__FILE__)) . '/libs/DBUtils.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>View Contact</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/contact-form.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="contacts.php">Home</a></li>
+          <li><a href="all_contacts.php">All Contacts</a></li>
+          <li><a href="index.php">Logout</a></li>
+        </ul>
+        <h3 class="text-muted">My Contacts</h3>
+      </div>
+      <h2>View Contact</h2>
+      <?php
+      Utils::showSessionMessage();
+      $id = isset($_GET['id']) ? $_GET['id'] : null;
+      ?>
+      <div class="alert alert-warning">You are about to remove this contact permanently. Do you want to continue?</div>
+      <div>
+        <a href="all_contacts.php" class="btn btn-success btn-lg active btn-block" role="button">No, don't remove the contact</a>
+        <a href="libs/controller.php?a=remove&id=<?php echo $id; ?>" class="btn btn-danger btn-lg active btn-block" role="button">Yes, remove this contact permanently</a>
+      </div>
+      <br>
+      <?php
+        
+        if(!isset($_GET['id']) || ($_GET['id'] == '')){
+          Utils::setSessionMessage('danger', 'An error occurred. Please try again.');
+          header('Location: '.ROOT_URL.'contacts.php');
+          exit();
+        }
+        $singleContact = Utils::getContactList($_GET['id']);
+        if($singleContact['STAT_TYPE'] != SC_SUCCESS_CODE){
+          Utils::setSessionMessage('danger', $singleContact['STAT_DESCRIPTION']);
+          Utils::showSessionMessage();
+          
+        }
+        else if ($singleContact['STAT_TYPE'] == SC_SUCCESS_CODE && $singleContact['DATA'] == null){
+          Utils::setSessionMessage('warning', $singleContact['STAT_DESCRIPTION']);
+          Utils::showSessionMessage();
+        }
+        else{
+          ?>
+      <div class="table-responsive">
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <td><?php echo isset($singleContact['DATA'][0]['name']) ? $singleContact['DATA'][0]['name'] : null; ?></td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td><?php echo isset($singleContact['DATA'][0]['email']) ? $singleContact['DATA'][0]['email'] : null; ?></td>
+            </tr>
+            <tr>
+              <th>Phone Number</th>
+              <td><?php echo isset($singleContact['DATA'][0]['phoneNumber']) ? $singleContact['DATA'][0]['phoneNumber'] : null; ?></td>
+            </tr>
+          </tr>
+          </tbody>
+       
+      </table>
+      </div>
+          <?php
+        }
+      ?>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ update_contact.php
@@ -0,0 +1,87 @@
+<?php
+require_once (dirname(__FILE__)) . '/libs/DBUtils.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Update Contact Details</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/contact-form.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="contacts.php">Home</a></li>
+          <li><a href="all_contacts.php">All Contacts</a></li>
+          <li><a href="index.php">Logout</a></li>
+        </ul>
+        <h3 class="text-muted">My Contacts</h3>
+      </div>
+      <?php
+      Utils::showSessionMessage();
+      $id = isset($_GET['id']) ? $_GET['id'] : null;
+      ?>
+
+      <form class="form-horizontal form-input" role="form" method="post" action="libs/controller.php?a=update&id=<?php echo $id; ?>">
+        <h2 class="form-input-heading">Update Contact Details</h2>
+        <div class="form-group">
+          <label for="name" class="col-sm-2 control-label">Name</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="name" id="name" placeholder="Name" value=<?php echo (isset($_GET['name'])) ? '"'.urldecode($_GET['name']).'"' : "" ?>>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="phone-number" class="col-sm-2 control-label">Phone Number</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="phoneNumber" id="phone-number" placeholder="Phone Number" value=<?php echo (isset($_GET['phoneNumber'])) ? '"'.urldecode($_GET['phoneNumber']).'"' : "" ?>>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="email" class="col-sm-2 control-label">Email</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="email" id="email" placeholder="Email" value=<?php echo (isset($_GET['email'])) ? '"'.urldecode($_GET['email']).'"' : "" ?>>
+          </div>
+        </div>
+        
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Save Contact</button>
+          </div>
+        </div>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
